e2110d03f69675558e06c25a0839b80e274806ec
jgarw <joegarwood@proton.me>
2025-04-01 20:39:49 -0400

    updated header

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show');
-        }
+        }});
+    }, {
+        threshold: 0.25 
     });
-}); 
 
 const scrollElements = document.querySelectorAll('.section');
 scrollElements.forEach((el) => observer.observe(el));
